32d34e7761aa9b7a284aba479d132d834955c038
Rachel <rstark.smith@gmail.com>
2022-11-16 16:30:56 -0600

    commit tree 823768889a5a56fcd147ffa78e2c1fed75e47b46

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -16,4 +16,16 @@ class BirdsController < ApplicationController
     end
   end
 
+  # POST /birds
+  def create 
+    bird = Bird.create(bird_params)
+    render json: bird, status: :created
+  end
+
+  private
+
+  def bird_params
+    params.permit(:name, :species)
+  end
+
 end
